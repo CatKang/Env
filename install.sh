4e47083d9a101408d34482ab9d6f8c1cbc3cb031
瀚之 <gary.wk@alibaba-inc.com>
2026-08-13 17:27:58 +0800

    link mktags to ~/bin in install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@ ln -s ~/Env/vim/vimrc .vimrc
 rm -rf .vim
 ln -s ~/Env/vim .vim
 
+# Soft link tools
+mkdir -p ~/bin
+ln -sf ~/Env/tools/mktags/mktags.sh ~/bin/mktags
+
 # Install Vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
